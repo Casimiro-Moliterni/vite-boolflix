7b8704c37b3e7b7d6cffe6d7fcff5eb2ebfb8320
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-04-17 15:40:59 +0200

    init:project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+export default {
+   
+  components:{
+    AppHeader
+  },
+  data(){
+    return{
+      store
+    }
+  }
+}
+</script>
+
+<template>
+ <AppHeader></AppHeader>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+import {store} from '../store.js'
+export default {
+    name:'AppHeader',
+    data(){
+        return{
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        {{ store.test }}
+    </header>
+</template>
+
+<style scoped lang="scss">
+</style>
